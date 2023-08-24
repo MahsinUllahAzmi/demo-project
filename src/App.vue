5eee029e7c7d5df44d4ac83bcc09742090560a1d
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2023-08-24 09:54:11 +0600

    to do design

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,69 @@
 <template>
-  <div class="shadow">
-
-    <div class="flex items-center justify-between py-6 px-6 bg-gray-50">
-      <div>
-        <h3 class="text-black/80 text-lg font-bold">Contact</h3>
-      </div>
-      <div class="flex gap-2 items-center">
-        <button class="border px-4 py-1 bg-blue-600 text-white rounded">
-          New Contact
-        </button>
-        <button class="flex items-center gap-2 border px-4 py-1 rounded text-black/70">
-          <PhFunnelSimple 
-            size="15"
-          />
-          Filter
-        </button>
-      </div>
+  <div class="max-w-[1000px] mx-auto">
+    <h1 class="text-center mt-5 text-lg font-bold mb-4">My Vue ToDo App</h1>
+    <!-- Input -->
+    <div class="flex mb-4">
+      <input 
+        type="text"
+        placeholder="Enner Task"
+        class="border py-1 px-4 w-full"
+        @input="text"
+      >
+      <button class="bg-red-500 border-none px-2 py-1 text-white text-sm font-semibold">
+        SUBMIT
+      </button>
     </div>
-
-    <table class="w-full">
-      <tr>
-        <th class="border-b py-4 px-4 text-left text-gray-500 text-sm font-medium">
-          <div class="flex items-center gap-2 justify-center ">
-            <span>Name</span>  
-            <PhArrowUp 
-              size="12"
-              weight="regular"
-            />
-          </div>
-        </th>
-        <th class="border-b py-4 px-4 text-left text-gray-500 text-sm font-medium">
-          <div class="flex items-center gap-2">
-            <span>Email</span>  
-            <PhArrowUp 
-              size="12"
-              weight="regular"
-            />
-          </div>
-        </th>
-        <th class="border-b py-4 px-4 text-left text-gray-500 text-sm font-medium">
-          <div class="flex items-center gap-2">
-            <span>Phone Number</span>  
-            <PhArrowUp 
-              size="12"
-              weight="regular"
-            />
-          </div>
-        </th>
-        <th class="border-b py-4 px-4 text-left text-gray-500 text-sm font-medium">
-          <div class="flex items-center gap-2">
-            <span>Tag</span>  
-            <PhArrowUp 
-              size="12"
-              weight="regular"
-            />
-          </div>
-        </th>
-      </tr>
-      <tr class="bg-gray-100">
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium flex items-center gap-6">
-          <input 
-            type="checkBox"
-            class="w-4 h-4"
-          >
-          <img 
-            src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" 
-            alt=""
-            class="w-10 aspect-square object-cover rounded-full"
-          >
-          Mahsin Ullah Azmi
-        </td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium">info@gmail.com</td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium">016 03283 901</td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium flex justify-between items-center">
-          Developer
-          <PhDotsThree 
-            size="20"
-            class="text-black/60"
-            weight="bold"
-          />
-        </td>
-      </tr>
-      <tr class="bg-gray-50">
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium flex items-center gap-6">
-          <input 
-            type="checkBox"
-            class="w-4 h-4"
-          >
-          <img 
-            src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" 
-            alt=""
-            class="w-10 aspect-square object-cover rounded-full"
-          >
-          Mahsin Ullah Azmi
-        </td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium">info@gmail.com</td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium">016 03283 901</td>
-        <td class="py-3 px-5 text-left text-black/60  text-sm font-medium flex justify-between items-center">
-          Developer
-          <PhDotsThree 
-            size="20"
-            class="text-black/60"
-            weight="bold"
-          />
-        </td>
-      </tr>
+      <!-- Task Table -->
+      
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr class="border">
+                <th scope="col" class="border px-6 py-3">
+                    Task
+                </th>
+                <th scope="col" class="border px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="border px-6 py-3">
+                    #
+                </th>
+                <th scope="col" class="border px-6 py-3">
+                    #
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border">
+                <td class="px-6 py-4 border whitespace-nowrap">
+                    item.task
+                </td>
+                <td class="px-6 py-4 border">
+                    item.status 
+                </td>
+                <td class="px-6 text-center py-4 border ">
+                    <span>
+                      <i class="ph ph-pencil-simple"></i>
+                    </span>
+                </td>
+                <td class="px-6 py-4 border text-center">
+                  <span>
+                      <i class="ph ph-trash"></i>
+                    </span>
+                </td>
+            </tr>
+        </tbody>
     </table>
-    <div class="flex justify-center">
-      <div class="bg-white py-4 px-6 flex items-center gap-4 text-sm font-medium">
-        <button class="bg-gray-300 p-1.5 rounded">
-          <PhCaretLeft 
-              size="18"
-              class="text-black/50"
-              weight="bold"
-          />
-        </button>
-        <button>
-          1
-        </button>
-        <button class="text-gray-400">
-          2
-        </button>
-        <button class="text-gray-400">
-          ....
-        </button>
-        <button class="text-gray-400">
-          8
-        </button>
-        <button class="bg-gray-300 p-1.5 rounded">
-          <PhCaretRight 
-              size="18"
-              class="text-black/50"
-              weight="bold"
-          />
-        </button>
-      </div>
-    </div>
+</div>
+
   </div>
-
-
 </template>
 
 <script setup>
-  import { PhDotsThree,PhArrowUp, PhCaretLeft,PhCaretRight, PhFunnelSimple } from "@phosphor-icons/vue";
+import { ref } from 'vue'
+
+const text = ref('')
+
+
+
 </script>
