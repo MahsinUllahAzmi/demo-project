72346fcd9edf292dbe33ab2dbad69e7877e99b24
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2023-08-30 23:39:09 +0600

    todo design

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,31 @@
 <template>
-  <div class="max-w-[1000px] mx-auto">
-    <h1 class="text-center mt-5 text-lg font-bold mb-4">My Vue ToDo App</h1>
-    <!-- Input -->
-    <div class="flex mb-4">
-      <input 
-        type="text"
-        placeholder="Enner Task"
-        class="border py-1 px-4 w-full"
-        @input="text"
-      >
-      <button class="bg-red-500 border-none px-2 py-1 text-white text-sm font-semibold">
-        SUBMIT
-      </button>
-    </div>
-      <!-- Task Table -->
-      
-<div class="relative overflow-x-auto">
-    <table class="w-full text-sm text-left text-gray-500">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr class="border">
-                <th scope="col" class="border px-6 py-3">
-                    Task
-                </th>
-                <th scope="col" class="border px-6 py-3">
-                    Status
-                </th>
-                <th scope="col" class="border px-6 py-3">
-                    #
-                </th>
-                <th scope="col" class="border px-6 py-3">
-                    #
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border">
-                <td class="px-6 py-4 border whitespace-nowrap">
-                    item.task
-                </td>
-                <td class="px-6 py-4 border">
-                    item.status 
-                </td>
-                <td class="px-6 text-center py-4 border ">
-                    <span>
-                      <i class="ph ph-pencil-simple"></i>
-                    </span>
-                </td>
-                <td class="px-6 py-4 border text-center">
-                  <span>
-                      <i class="ph ph-trash"></i>
-                    </span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+  <button @click="add" class="bg-red-500 text-white py-1 px-4 rounded">click me</button>
 
-  </div>
+  <p id="demo">
+   
+  </p>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+ const mahsin = [
+    {
+      name: 'Muhibbullah',
+      age: 28
+    },
+    {
+      name: 'Aahsin ullah',
+      age: 20
+    },
+    {
+      name: 'Baidullah',
+      age: 10,
+    },
+  
+  ]
 
-const text = ref('')
+  function add(){
+    console.log(mahsin.shift());
+  }
 
 
 
---
+++ src/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="mb-8">
+    <input v-model="inputString" class="w-[400px] border border-black/20 py-1 px-4">
+    <button @click="addItem" class="bg-red-500 text-white py-1 px-4 rounded">Add ToDo</button>
+  </div>
+
+  <div>
+    <p v-for="(item, index) in ToDo" :class="item.status ? 'text-red-500' : ''">
+      {{item.title}}
+      <button @click="item.status = true" class="py-1 px-1 bg-green-500 text-white rounded">Done</button>
+      <button @click="removeItem(index)" class="py-1 px-1 bg-red-500 text-white rounded">X</button>
+    </p>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const inputString = ref('')
+
+let ToDo = ref([])
+
+const addItem = () => {
+  ToDo.value.push({
+    id: ToDo.value.length + 1,
+    title: inputString.value,
+    status: false,
+  })
+  inputString.value = null
+}
+
+const removeItem = (index) => {
+  ToDo.value.splice(index, 1)
+}
+
+</script>
